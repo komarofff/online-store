e72f6f207817c10d3784ceed58c532e584e3b6c8
Anatoliy Komarov <admin@komaroff.biz>
2022-12-22 17:53:34 +0300

    feat: add pagination script for cart page

--- src/components/cart/ItemsInCart.vue
+++ src/components/cart/ItemsInCart.vue
@@ -1,4 +1,8 @@
 <template>
+  max-pages {{ maxPage }}
+  <ul>
+    <li v-for="i in maxPage" :key="i" @click="goToPage(i)">{{ i }}</li>
+  </ul>
   {{ itemsInCart }}
 </template>
 
@@ -10,13 +14,48 @@ export default {
   data() {
     return {
       itemsInCart: [],
+      page: 1,
+      maxPage: 1,
+      itemsPerPage: 3,
+      startQuery: {},
     };
   },
   mounted() {
     this.itemsInCart = this.getCartArray;
+    this.maxPage = Math.ceil(this.itemsInCart.length / this.itemsPerPage);
+    this.startQuery = this.$route.query.page;
+    if (this.startQuery && this.startQuery > 0) {
+      console.log("start with query parameters", this.startQuery);
+      this.showPaginatedItems(this.startQuery);
+    } else {
+      this.showAllItems();
+    }
   },
+  watch: {},
   computed: {
     ...mapGetters("Cart", ["getCartArray"]),
+  },
+  methods: {
+    goToPage(val) {
+      let queries = JSON.parse(JSON.stringify(this.$route.query));
+      if (val) {
+        queries.page = val;
+        this.$router.replace({ query: queries });
+      } else {
+        delete queries.page;
+        this.$router.replace({ query: queries });
+      }
+      this.showPaginatedItems(val);
+    },
+    showAllItems() {
+      this.itemsInCart = this.getCartArray;
+    },
+    showPaginatedItems(page) {
+      let start = page * this.itemsPerPage - this.itemsPerPage;
+      let end = page * this.itemsPerPage;
+      console.log("start: " + start + " end: " + end);
+      this.itemsInCart = this.getCartArray.slice(start, end);
+    },
   },
 };
 </script>
