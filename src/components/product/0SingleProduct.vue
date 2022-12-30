7cb24f171e6bfc2ac1a7475f6b07d78c66d33f84
Anatoliy Komarov <admin@komaroff.biz>
2022-12-30 13:12:25 +0300

    feat: add viewed products block for single product

--- src/components/product/0SingleProduct.vue
+++ src/components/product/0SingleProduct.vue
@@ -1,97 +1,125 @@
 <template>
   <img v-if="isLoader" src="@/assets/loader.gif" alt="loader" />
-  <section class="container" v-if="isCatHere">
+
+  <section class="container" v-if="getSingleProduct">
     <div class="main__home-content-product">
       <div class="home-menu-crumbs">
-        <p class="crumbs-start">{{ getSingleProduct[0].title }}</p>
+        <p class="crumbs-start">{{ getSingleProduct.title }}</p>
         <div class="crumbs-address">
-          <a class="img-address" href=""
+          <router-link class="img-address" :to="{ name: 'home' }"
             ><img src="@/assets/icon/address-svg.svg" alt=""
-          /></a>
-          <a href="">Catalog</a>
+          /></router-link>
           <img
             class="img-arrow"
             src="@/assets/icon/arrow-link-right.svg"
             alt=""
           />
-          <a href="">{{ getSingleProduct[0].category }}</a>
+          <router-link :to="{ name: 'categories' }">Catalog</router-link>
           <img
             class="img-arrow"
             src="@/assets/icon/arrow-link-right.svg"
             alt=""
           />
-          <a href="">{{ getSingleProduct[0].title }}</a>
+          <router-link
+            class="capitalize"
+            :to="{ path: `/catalog/${getSingleProduct.category}` }"
+            >{{ getSingleProduct.category }}</router-link
+          >
+          <img
+            class="img-arrow"
+            src="@/assets/icon/arrow-link-right.svg"
+            alt=""
+          />
+          <p>{{ getSingleProduct.title }}</p>
         </div>
       </div>
 
       <div class="product">
         <img v-if="isLoader" src="@/assets/loader.gif" alt="loader" />
-        <p class="product__brand">{{ getSingleProduct[0].brand }}</p>
-        <p class="product__link">
-          Shop <img src="@/assets/icon/arrow-link-right.svg" alt="" />
+        <!--        <p class="product__brand">{{ getSingleProduct.brand }}</p>-->
+        <p class="product__link capitalize">
+          {{ getSingleProduct.brand }}
         </p>
 
         <div class="product__content">
           <div class="product__photo-main">
             <div class="product__photo-big">
               <img
-                :src="getSingleProduct[0].thumbnail"
-                :alt="getSingleProduct[0].title"
+                class="fade-innn"
+                :src="bigImage"
+                :alt="getSingleProduct.title"
               />
             </div>
             <div class="product__photo-small">
               <div
                 class="product__photo-small-div"
-                v-for="image in getSingleProduct[0].images"
+                :class="{ active: isBigImage(getSingleProduct.thumbnail) }"
+              >
+                <img
+                  @click="changeBigPhoto(getSingleProduct.thumbnail)"
+                  :src="getSingleProduct.thumbnail"
+                  :alt="getSingleProduct.title"
+                />
+              </div>
+              <div
+                class="product__photo-small-div"
+                :class="{ active: isBigImage(image) }"
+                v-for="image in getSingleProduct.images"
                 :key="image"
               >
-                <img :src="image" :alt="getSingleProduct[0].title" />
+                <img
+                  @click="changeBigPhoto(image)"
+                  :src="image"
+                  :alt="getSingleProduct.title"
+                />
               </div>
 
-              <div class="product__photo-small-false">+2</div>
+              <!--              <div class="product__photo-small-false">+2</div>-->
             </div>
           </div>
 
           <div class="product__info">
-            <p class="product__name">{{ getSingleProduct[0].title }}</p>
+            <p class="product__name">{{ getSingleProduct.title }}</p>
             <p class="product__description">
-              {{ getSingleProduct[0].description }}
+              {{ getSingleProduct.description }}
             </p>
             <p class="product__brand-name">
-              Brand: <span>{{ getSingleProduct[0].brand }}</span>
+              <strong>Brand: </strong>
+              <span class="uppercase">{{ getSingleProduct.brand }}</span>
             </p>
             <p class="product__category">
-              Category: <span>{{ getSingleProduct[0].category }}</span>
+              <strong>Category: </strong>
+              <span class="uppercase">{{ getSingleProduct.category }}</span>
             </p>
             <p class="product__rating">
-              Rating:
+              <strong>Rating: </strong>
               <span class="product__rating-score">{{
-                getSingleProduct[0].rating
+                getSingleProduct.rating
               }}</span>
             </p>
             <p class="product__stock">
-              Stock: <span>{{ getSingleProduct[0].stock }}</span>
+              <strong>Stock: </strong> <span>{{ getSingleProduct.stock }}</span>
             </p>
-            <p class="product__price">€{{ getSingleProduct[0].price }}</p>
+            <p class="product__price">€{{ getSingleProduct.price }}</p>
 
             <div class="product__btn">
               <button
                 class="product__button btn-pay"
-                v-if="!isActive(getSingleProduct[0].id)"
-                @click="addToCart(getSingleProduct[0])"
+                v-if="!isActive(getSingleProduct.id)"
+                @click="addToCart(getSingleProduct)"
               >
                 ADD TO CART
               </button>
               <button
                 class="product__button btn-del"
-                v-if="isActive(getSingleProduct[0].id)"
-                @click="delCart(getSingleProduct[0].id)"
+                v-if="isActive(getSingleProduct.id)"
+                @click="delCart(getSingleProduct.id)"
               >
                 DROP FROM CART
               </button>
               <button
                 class="product__button btn-pay"
-                @click="openCheckout(getSingleProduct[0])"
+                @click="openCheckout(getSingleProduct)"
               >
                 BUY NOW
               </button>
@@ -103,79 +131,74 @@
   </section>
 </template>
 
-<script>
+<script lang="ts">
 import { mapActions, mapGetters } from "vuex";
-
-export default {
-  props: ["cat", "id"],
+import { ProdArr } from "@/store/modules/Filter";
+import { CartArr } from "@/store/modules/Cart";
+import { defineComponent } from "vue";
+export default defineComponent({
+  props: ["id"],
   data() {
     return {
-      isLoader: false,
-      isCatHere: false,
+      isLoader: false as boolean,
+      bigImage: "" as string,
     };
   },
 
   async mounted() {
     this.isLoader = true;
-    await this.getSingleCat(this.cat);
-    console.log(!isNaN(+this.id), parseInt(this.id));
-    if (!isNaN(+this.id) && parseInt(this.id) > 0 && parseInt(this.id) < 100) {
+    // console.log("this.id", this.id);
+    //await this.getSingleCat(this.cat);
+    if (!isNaN(+this.id) && parseInt(this.id) > 0 && parseInt(this.id) < 101) {
       await this.getSingleProd(Number(this.id));
+      this.bigImage = this.getSingleProduct.thumbnail;
     } else {
-      console.log("error-1");
-      this.$router.push({ name: "error" });
+      this.$router.push({ name: "error", params: { pathMatch: this.id } });
     }
-
-    this.isLoader = false;
-
-    // if (
-    //   this.id === 0 ||
-    //   this.getSingleCategory.products.some((el) => el.id === this.id) === false
-    // ) {
-    //   console.log("false id");
-    //   this.$router.push({ name: "error" });
-    // }
-
-    this.isCatHere = this.getSingleCategory.every((el) => {
-      return el.category === this.getSingleProduct[0].category;
-    });
-    if (
-      this.getSingleProduct[0].category !== this.cat ||
-      this.isCatHere === false
-    ) {
-      // this.$router.push({ name: "error" });
+    if (!this.getSingleProduct) {
+      this.$router.push({ name: "error", params: { pathMatch: this.id } });
+    } else {
+      this.isLoader = false;
     }
-
-    //return this.$router.push({ name: "error" });
   },
-  watch: {},
+  watch: {
+    // getSingleProduct() {
+    //
+    //   return this.getSingleProduct;
+    // },
+  },
   computed: {
-    ...mapGetters("Filter", ["getSingleCategory", "getSingleProduct"]),
+    ...mapGetters("Filter", ["getSingleProduct"]),
     ...mapGetters("Cart", ["getCartArray"]),
   },
   methods: {
-    ...mapActions("Filter", ["getSingleCat", "getSingleProd"]),
+    ...mapActions("Filter", ["getSingleProd"]),
     ...mapActions("Cart", ["pushToCart", "delFromCart"]),
-
-    isActive(val) {
-      return this.getCartArray.find((product) => {
+    changeBigPhoto(src: string) {
+      this.bigImage = src;
+    },
+    isBigImage(src: string) {
+      return src === this.bigImage;
+    },
+    isActive(val: number) {
+      return this.getCartArray.find((product: ProdArr) => {
         return product.id === val;
       });
     },
-    async addToCart(val) {
+    async addToCart(val: CartArr) {
       val.quantity = 1;
       await this.pushToCart(val);
     },
-    async delCart(val) {
+    async delCart(val: number) {
       await this.delFromCart(val);
     },
-    async openCheckout(val) {
+    async openCheckout(val: CartArr) {
       val.quantity = 1;
       await this.pushToCart(val);
       this.$router.push({ name: "cart", query: { byeNow: "true" } });
     },
   },
-};
+});
 </script>
 
-<style scoped></style>
+<style lang="scss"></style>
--- src/components/product/SingleProduct.vue
+++ src/components/product/SingleProduct.vue
@@ -23,8 +23,8 @@
           <router-link
             class="capitalize"
             :to="{ path: `/catalog/${getSingleProduct.category}` }"
-            >{{ getSingleProduct.category }}</router-link
-          >
+            >{{ getSingleProduct.category }}
+          </router-link>
           <img
             class="img-arrow"
             src="@/assets/icon/arrow-link-right.svg"
@@ -33,7 +33,6 @@
           <p>{{ getSingleProduct.title }}</p>
         </div>
       </div>
-
       <div class="product">
         <img v-if="isLoader" src="@/assets/loader.gif" alt="loader" />
         <!--        <p class="product__brand">{{ getSingleProduct.brand }}</p>-->
@@ -127,21 +126,117 @@
           </div>
         </div>
       </div>
+
+      <div class="swiper__box" v-if="viewedProducts.length > 0">
+        <div class="swiper__box-title">
+          Viewed Products
+          <div class="myPrev"></div>
+          <div class="myNext"></div>
+        </div>
+        <swiper
+          :slidesPerView="1"
+          :spaceBetween="10"
+          :freeMode="true"
+          :navigation="{ enabled: true, prevEl: '.myPrev', nextEl: '.myNext' }"
+          :modules="modules"
+          :breakpoints="{
+            '540': {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            '768': {
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+            '1024': {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+            '1280': {
+              slidesPerView: 5,
+              spaceBetween: 20,
+            },
+          }"
+          class="mySwiper"
+        >
+          <swiper-slide v-for="product in viewedProducts" :key="product.id">
+            <div class="card-item card-hover">
+              <div class="card-photo">
+                <router-link :to="`/product-details/${product.id}`"
+                  ><img
+                    class="card-img"
+                    :src="product.thumbnail"
+                    :alt="product.title"
+                /></router-link>
+              </div>
+              <div class="card-info">
+                <p class="card-name">
+                  <span>{{ product.title }}</span>
+                </p>
+                <p class="card-brand">
+                  Brand: <span>{{ product.brand }}</span>
+                </p>
+                <p class="card-category">
+                  Category: <span>{{ product.category }}</span>
+                </p>
+                <p class="card-rating">
+                  Rating: <span>{{ product.rating }}</span>
+                </p>
+                <p class="card-stock">
+                  Stock: <span>{{ product.stock }}</span>
+                </p>
+
+                <div class="price-container">
+                  <div class="price-price">
+                    <span class="card-price">€{{ product.price }}</span>
+                    <span class="card-discount"
+                      >-{{ product.discountPercentage }}%</span
+                    >
+                  </div>
+                </div>
+                <div class="card-btn-container">
+                  <div class="card-link-more">
+                    <router-link :to="`/product-details/${product.id}`"
+                      >More Info
+                    </router-link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
     </div>
   </section>
 </template>
 
 <script lang="ts">
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/free-mode";
+
+// import required modules
+import { FreeMode, Navigation } from "swiper";
+
 import { mapActions, mapGetters } from "vuex";
 import { ProdArr } from "@/store/modules/Filter";
 import { CartArr } from "@/store/modules/Cart";
 import { defineComponent } from "vue";
+
+export interface IStorageViewed {
+  id: number;
+}
+
 export default defineComponent({
   props: ["id"],
   data() {
     return {
       isLoader: false as boolean,
       bigImage: "" as string,
+      viewedProducts: [] as string[],
     };
   },
 
@@ -160,20 +255,52 @@ export default defineComponent({
     } else {
       this.isLoader = false;
     }
+    this.pushToViewed();
   },
   watch: {
-    // getSingleProduct() {
-    //
-    //   return this.getSingleProduct;
-    // },
+    async id() {
+      await this.getSingleProd(Number(this.id));
+      this.bigImage = this.getSingleProduct.thumbnail;
+      window.scrollTo(0, 0);
+    },
   },
   computed: {
-    ...mapGetters("Filter", ["getSingleProduct"]),
+    ...mapGetters("Filter", ["getSingleProduct", "getAllProducts"]),
     ...mapGetters("Cart", ["getCartArray"]),
   },
   methods: {
     ...mapActions("Filter", ["getSingleProd"]),
     ...mapActions("Cart", ["pushToCart", "delFromCart"]),
+    pushToViewed() {
+      if (
+        localStorage.getItem("viewed") &&
+        JSON.parse(localStorage.getItem("viewed") || "").length
+      ) {
+        let fromStorage = JSON.parse(localStorage.getItem("viewed") || "");
+        this.viewedProducts = this.getAllProducts.filter((el: ProdArr) => {
+          if (fromStorage.find((elem: IStorageViewed) => elem.id === el.id)) {
+            return el;
+          }
+        });
+        if (
+          !fromStorage.find(
+            (el: IStorageViewed) => el.id === this.getSingleProduct.id
+          )
+        ) {
+          if (fromStorage.length >= 10) {
+            fromStorage.unshift({ id: this.getSingleProduct.id });
+            fromStorage.pop();
+          } else {
+            fromStorage.push({ id: this.getSingleProduct.id });
+          }
+          localStorage.setItem("viewed", JSON.stringify(fromStorage));
+        }
+      } else {
+        let fromStorage = [] as IStorageViewed[];
+        fromStorage.unshift({ id: this.getSingleProduct.id });
+        localStorage.setItem("viewed", JSON.stringify(fromStorage));
+      }
+    },
     changeBigPhoto(src: string) {
       this.bigImage = src;
     },
@@ -198,7 +325,118 @@ export default defineComponent({
       this.$router.push({ name: "cart", query: { byeNow: "true" } });
     },
   },
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [FreeMode, Navigation],
+    };
+  },
 });
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.swiper__box {
+  padding: 10px 5px;
+  margin: 15px 0;
+  background: #fff;
+  border-radius: 15px;
+  .price-container {
+    justify-content: center;
+  }
+  .card-info {
+    align-self: stretch;
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    flex-grow: 1;
+  }
+}
+
+.swiper__box-title {
+  background: #fff;
+  padding: 5px 50px 2px;
+  font-size: 18px;
+  font-weight: bold;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  position: relative;
+  border-bottom: 1px solid #ede7f6;
+  margin-bottom: 10px;
+}
+
+.swiper {
+  width: 100%;
+  height: 100%;
+}
+
+.swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: unset !important;
+  min-height: 100% !important;
+
+  .card-item {
+    max-width: unset;
+    width: 250px;
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+}
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.myPrev,
+.myNext {
+  position: absolute;
+  top: 5px;
+  width: 20px;
+  height: 20px;
+  z-index: 10;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #2196f3;
+  transition: 0.3s linear;
+  &:hover {
+    background-color: rgba(33, 150, 243, 0.5);
+  }
+}
+
+.myPrev {
+  right: 20px;
+  background-image: url(~@/assets/icon/arrow-link-right.svg);
+  background-repeat: no-repeat;
+  transform: rotate(180deg);
+  background-position: center;
+  z-index: 10;
+}
+
+.myNext {
+  right: 2px;
+  background-image: url(~@/assets/icon/arrow-link-right.svg);
+  background-repeat: no-repeat;
+  background-position: center;
+  z-index: 10;
+}
+.swiper-button-disabled {
+  background-color: rgba(204, 204, 204, 0.3);
+  z-index: 9;
+}
+</style>
